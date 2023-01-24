97e5d0694888d8df910d2e4a4671ba4ea48b7bb9
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-01-24 19:36:24 +0100

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -42,10 +42,10 @@ class DiscordClient
 	static function onReady()
 	{
 		DiscordRpc.presence({
-			details: "In the Menus",
+			details: "Playing Mods Logger",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageText: "Vs Logger"
 		});
 	}
 
@@ -82,7 +82,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
+			largeImageText: "Logger Version: " + MainMenuState.LoggerVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
